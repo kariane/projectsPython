c3a923976b83553276bb6f6121c5353f83e8b16b
Kariane Coura <karianecoura123@gmail.com>
2020-10-09 18:49:25 -0300

    newprojects

---
+++ project3.py
@@ -0,0 +1,5 @@
+v=input('enter a plural noun:')
+a=input('enter an adjective:')
+sn=input('enter a noun:')
+ad=input('enter an adjective:')
+print(f'Being stuck at home during a/an {sn} -storm is the worst! My {ad} brother Charlie has been banging on his {v} all day. It is driving me totally {a}, and I am not the only one!')
